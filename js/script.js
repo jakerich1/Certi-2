e82e018b3e77475338eb25d7c67791100d2ecea1
jakerich1 <jakeriches1@gmail.com>
2021-05-01 18:22:06 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,8 +40,12 @@ const pausebtn = document.querySelector('#pause')
 const scrubArea = document.querySelector('.scrubArea')
 const scrubBar = document.querySelector('.scrubTop')
 const botNav = document.querySelector('.botNav')
-pausebtn.style.display = "none"
+const position = {
+    x: 0,
+    y: 0
+}
 const screenWidth = window.innerWidth
+pausebtn.style.display = "none"
 
 function playAudio() { 
     audio.play(); 
@@ -65,23 +69,19 @@ function scrubClick(event){
 
 playbtn.addEventListener("click", playAudio)
 pausebtn.addEventListener("click", pauseAudio)
+scrubArea.addEventListener("click", scrubClick)
+
 audio.onended = function() {
     pausebtn.style.display = "none"
     playbtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-repeat" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 12v-3a3 3 0 0 1 3 -3h13m-3 -3l3 3l-3 3"></path><path d="M20 12v3a3 3 0 0 1 -3 3h-13m3 3l-3 -3l3 -3"></path></svg>'
     playbtn.style.display = "inline-block"
 };
+
 audio.ontimeupdate = function() {
     let progress = (audio.currentTime/audio.duration)*100
     console.log(progress)
     scrubBar.style.width = `${progress}%`
 };
-scrubArea.addEventListener("click", scrubClick)
-
-//
-const position = {
-    x: 0,
-    y: 0
-}
 
 interact('.draggable').draggable({
     listeners: {
@@ -89,12 +89,10 @@ interact('.draggable').draggable({
             event.target.style.transition = "unset"
         },
         move(event) {
-            let rotateDeg = (position.x/screenWidth)*30;
+            let rotateDeg = (position.x/screenWidth)*20;
             position.x += event.dx
             position.y += event.dy
-
             event.target.style.transform = `translate(${position.x}px, ${position.y}px) rotate(${rotateDeg}deg)`
-            
         },
         end(event) {
             event.target.style.transition = "all 0.3s ease-in-out"
